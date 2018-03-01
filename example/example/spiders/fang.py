0b8c82c28ddcad4010aff97c6192dcef05f3e660
John <158227990@qq.com>
2018-03-02 01:35:30 +0800

    update

--- example/example/spiders/fang.py
+++ example/example/spiders/fang.py
@@ -7,16 +7,23 @@ from scrapy.spiders import CrawlSpider, Rule
 class FangSpider(CrawlSpider):
     name = 'fang'
     allowed_domains = ['esf.cd.fang.com']
-    start_urls = ['http://esf.cd.fang.com/']
+    start_urls = ['http://esf.cd.fang.com/chengjiao/h37/']
 
     rules = (
-        Rule(LinkExtractor(allow=r'chushou/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=r'chengjiao'), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
-        i = {}
-        print 'hello world'
+        url = response.url
+        #titles = response.xpath('//dl/dd/p[1]/a/text()').extract()
+
+        items = response.xpath('//dl')
+
+        for index, item in enumerate(items):
+            name = item.xpath('dd/p[1]/a/text()').extract()
+            print(name)
+
         #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
         #i['name'] = response.xpath('//div[@id="name"]').extract()
         #i['description'] = response.xpath('//div[@id="description"]').extract()
-        return i
+
