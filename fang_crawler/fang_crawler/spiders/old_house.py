5303ae32d698ff5d54500d82384eb72dd236d2a2
John Shi <john.shi@emc.com>
2018-03-11 19:27:00 +0800

    update

--- fang_crawler/fang_crawler/spiders/old_house.py
+++ fang_crawler/fang_crawler/spiders/old_house.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from fang_crawler.items import FangCrawlerItem
 
 class OldHouseSpider(scrapy.Spider):
     name = "old_house"
-    allowed_domains = ["esf.cd.fang.com/chengjiao/h37/"]
-    start_urls = ['http://esf.cd.fang.com/chengjiao/h37//']
+    allowed_domains = ["esf.cd.fang.com/chengjiao/h37"]
+    start_urls = ['http://esf.cd.fang.com/chengjiao/h37/']
 
     def parse(self, response):
+        connections = response.xpath('//dl[@id]')
+
+        for connection in connections:
+            item = FangCrawlerItem()
+            item['name'] = connection.xpath('dd/p[1]/a/text()').extract()[0].strip()
+            item['total_price'] = connection.xpath('dd/div[3]/p[1]/span[1]/text()').extract()[0].strip()
+            item['average_price'] = connection.xpath('dd/div[3]/p[2]/b/text()').extract()[0].strip()
+            yield item
+
         pass
